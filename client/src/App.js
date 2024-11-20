e331e279125854e53bb363a0f5029d554e37732b
huybaoo <156101571+huybaoo@users.noreply.github.com>
2024-11-20 18:20:25 +0700

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,15 @@ import Login from './components/Login';
 import Cart from './components/Cart';
 import Menu from './components/Menu';
 import Register from './components/Register';
+import AdminCategory from './components/AdminCategory';
+import AdminProductList from './components/AdminProductList';
+import SearchResults from './components/SearchResults';
+import LaptopProductList from './components/LaptopProductList';
+import GearProductList from './components/GearProductList';
+import PkbgProductList from './components/PkbgProductList';
+import LinhkienProductList from './components/LinhkienProductList';
+import ManhinhProductList from './components/ManhinhProductList';
+import AdminHome from './components/AdminHome';
 
 const App = () => {
     return (
@@ -22,6 +31,16 @@ const App = () => {
                 <Route path="/register" element={<Register />} />
                 <Route path="/productlist" element={<ProductList />} />
                 <Route path="/product/:id" element={<ProductDetail />} />
+                <Route path="/laptop" element={<LaptopProductList />}/>
+                <Route path="/manhinh"  element={<ManhinhProductList />}/>
+                <Route path="/gear"  element={<GearProductList />}/>
+                <Route path="/linhkien"  element={<LinhkienProductList />}/>
+                <Route path="/phukienbanghe"  element={<PkbgProductList />}/>
+                <Route path="/search" element={<SearchResults />} />
+
+                <Route path="/admin" element={<AdminHome />}/>
+                <Route path="/admin/admincategory" element={<AdminCategory />}/>
+                <Route path="/admin/adminproductlist" element={<AdminProductList />}/>
             </Routes>
         </Router>
     );
