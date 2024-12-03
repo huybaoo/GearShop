8b7fd7d24fac35c0aa4540668426e7b2f1031516
huybaoo <156101571+huybaoo@users.noreply.github.com>
2024-12-03 15:19:07 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const vnpayRouter = require('./routes/vnpay'); 
+const vnpayRouter = require('./vnpay'); 
 // Sử dụng router VNPAY
 app.use('/api/v1/vnpay', vnpayRouter);
 
